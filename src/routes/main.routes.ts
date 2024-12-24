5126869fb7d195638a3cf7ac68d0b4072b17230d
PramisGrg <gurungpramis361@gmail.com>
2024-12-24 20:48:36 +0545

    feat: nonFound

--- src/routes/main.routes.ts
+++ src/routes/main.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import userRouter from "./user.routes";
 import authRoutes from "./auth.routes";
 import todoRoutes from "./todo.routes";
 import { authMiddleware } from "../middlewares/auth.middleware";
@@ -7,7 +6,6 @@ import { authMiddleware } from "../middlewares/auth.middleware";
 const mainRouter = Router();
 
 mainRouter.use("/auth", authRoutes);
-mainRouter.use("/user", userRouter);
 mainRouter.use("/todo", authMiddleware, todoRoutes);
 
 export default mainRouter;
--- src/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { createUser } from "../controller/user.controller";
-
-const userRouter = Router();
-
-userRouter.post("/", createUser);
-
-export default userRouter;
