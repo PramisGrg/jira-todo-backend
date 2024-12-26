2bc4fbd0184e84ead9ff6f1da2c2ef33cc05c9ff
PramisGrg <gurungpramis361@gmail.com>
2024-12-26 16:20:11 +0545

    fix: dist added so it could be deployed

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("dotenv/config");
@@ -10,17 +12,17 @@ const not_found_1 = __importDefault(require("./utils/not.found"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const corsOptions = {
-    origin: [
-        "http://localhost:5173",
-        "https://todo-app-drag-and-drop-gilt.vercel.app",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
-    credentials: true,
-    optionsSuccessStatus: 204,
-    exposedHeaders: ["set-cookie"],
-    preflightContinue: false,
-    maxAge: 86400,
+  origin: [
+    "http://localhost:5173",
+    "https://todo-app-drag-and-drop-gilt.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  credentials: true,
+  optionsSuccessStatus: 204,
+  exposedHeaders: ["set-cookie"],
+  preflightContinue: false,
+  maxAge: 86400,
 };
 app.use((0, cors_1.default)(corsOptions));
 app.options("*", (0, cors_1.default)(corsOptions));
@@ -30,5 +32,5 @@ app.use(main_routes_1.default);
 app.use("*", not_found_1.default);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`backend running on port ${PORT}`);
+  console.log(`backend running on port ${PORT}`);
 });
